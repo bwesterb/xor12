99533171bd45ddd9a958d8d8ef427db2e8318258
Bas Westerbaan <bas@westerbaan.name>
2023-04-29 21:25:18 +0200

    Convert to TCP proxy

--- main.go
+++ main.go
@@ -1,26 +1,51 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
+	"io"
+	"log"
+	"net"
 
 	"github.com/cloudflare/circl/xof/k12"
 	"github.com/templexxx/xor"
 )
 
-func main() {
-	var buf [8192]byte
-	var mask [8192]byte
-	h := k12.NewDraft10([]byte{})
+var (
+	listenAddr   = flag.String("l", ":42422", "listen addr")
+	upstreamAddr = flag.String("u", "", "upstream addr")
+	key          = flag.String("k", "", "key")
+)
 
-	if len(os.Args) >= 2 {
-		_, _ = h.Write([]byte(os.Args[1]))
+func main() {
+	flag.Parse()
+
+	ln, err := net.Listen("tcp", *listenAddr)
+	if err != nil {
+		log.Fatalf("Listen: %v", err)
 	}
 
 	for {
-		n, err := os.Stdin.Read(buf[:])
+		conn, err := ln.Accept()
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Stdin.Read: %v", err)
+			log.Fatalf("Accept: %v", err)
+		}
+
+		go handle(conn)
+	}
+}
+
+func pump(ctx string, r io.Reader, w io.Writer) {
+	var buf [8192]byte
+	var mask [8192]byte
+
+	h := k12.NewDraft10([]byte{})
+	_, _ = h.Write([]byte(*key))
+
+	for {
+		n, err := r.Read(buf[:])
+		if err != nil {
+			log.Printf("%s %v", ctx, err)
 			return
 		}
 
@@ -28,10 +53,21 @@ func main() {
 
 		xor.BytesSameLen(buf[:n], buf[:n], mask[:n])
 
-		_, err = os.Stdout.Write(buf[:n])
+		_, err = w.Write(buf[:n])
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Stdout.Write: %v", err)
+			log.Printf("%s %v", ctx, err)
 			return
 		}
 	}
+}
+
+func handle(conn net.Conn) {
+	up, err := net.Dial("tcp", *upstreamAddr)
+	if err != nil {
+		log.Printf("%s %v", conn.RemoteAddr(), err)
+		return
+	}
+
+	go pump(fmt.Sprintf("%v up->down", conn.RemoteAddr()), up, conn)
+	go pump(fmt.Sprintf("%v down->up", conn.RemoteAddr()), conn, up)
 }
